f83c22c98d2f4ef2e72e1249baf0f1c8b6ebdbdf
Christina Blye <christinablye123@gmail.com>
2026-03-16 22:47:05 -0400

    Completed login authentication and file upload

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired
 
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
+    submit = SubmitField('Login')
+
+
+class UploadForm(FlaskForm):
+    photo = FileField(
+        'Upload Photo',
+        validators=[
+            FileRequired(),
+            FileAllowed(['jpg', 'jpeg', 'png'], 'Images only!')
+        ]
+    )
+    submit = SubmitField('Upload')
