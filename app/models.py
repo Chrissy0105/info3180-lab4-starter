65d7e79913994acf14be4bc02b15d8af4b9de350
Christina Blye <christinablye123@gmail.com>
2026-03-16 22:22:33 -0400

    Added password hashing and test user

--- app/models.py
+++ app/models.py
@@ -1,17 +1,22 @@
 from . import db
+from werkzeug.security import generate_password_hash
 
 
 class UserProfile(db.Model):
-    # You can use this to change the table name. The default convention is to use
-    # the class name. In this case a class name of UserProfile would create a
-    # user_profile (singular) table, but if we specify __tablename__ we can change it
-    # to `user_profiles` (plural) or some other name.
+
     __tablename__ = 'user_profiles'
 
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80))
     last_name = db.Column(db.String(80))
     username = db.Column(db.String(80), unique=True)
+    pwd_hash = db.Column(db.String(255))
+
+    def __init__(self, first_name, last_name, username, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.pwd_hash = generate_password_hash(password)
 
     def is_authenticated(self):
         return True
@@ -24,9 +29,9 @@ class UserProfile(db.Model):
 
     def get_id(self):
         try:
-            return unicode(self.id)  # python 2 support
+            return unicode(self.id)
         except NameError:
-            return str(self.id)  # python 3 support
+            return str(self.id)
 
     def __repr__(self):
         return '<User %r>' % (self.username)
